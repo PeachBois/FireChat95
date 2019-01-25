344cbbaa55c2d7a5b497e9efadac4e86767ff5c0
Edwin Kim <edwin.k.kim@gmail.com>
2019-01-25 13:55:08 -0600

    Merged with noahs branch

--- client/components/messageBox.js
+++ client/components/messageBox.js
@@ -1,74 +1,74 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { withRouter, Route, Switch } from 'react-router-dom'
-import { withFirebase } from '../Firebase/index'
-import { compose } from 'recompose'
-import firebase from 'firebase'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter, Route, Switch } from 'react-router-dom';
+import { withFirebase } from '../Firebase/index';
+import { compose } from 'recompose';
+import firebase from 'firebase';
 
 class messageBox extends Component {
-  constructor () {
-    super()
+  constructor() {
+    super();
     this.state = {
       username: '',
       body: '',
       postList: []
-    }
+    };
   }
-  componentDidMount () {
-    let postList = []
+  componentDidMount() {
+    let postList = [];
     const dbRefObject = firebase
       .database()
       .ref()
-      .child('posts')
+      .child('posts');
 
     dbRefObject.on('value', snap => {
-      postList = []
-      const postObj = snap.val()
-      let key = Object.keys(postObj)
+      postList = [];
+      const postObj = snap.val();
+      let key = Object.keys(postObj);
       for (key in postObj) {
-        postList.push(postObj[key])
+        postList.push(postObj[key]);
       }
-      this.setState({ postList, username: this.props.username })
-    })
+      this.setState({ postList, username: this.props.username });
+    });
   }
 
   handleChange = evt => {
-    this.setState({ [evt.target.name]: evt.target.value })
-  }
+    this.setState({ [evt.target.name]: evt.target.value });
+  };
   handleSubmit = evt => {
-    this.props.firebase.writeNewPost(this.state.username, this.state.body)
-  }
+    this.props.firebase.writeNewPost(this.state.username, this.state.body);
+  };
 
-  render () {
-    let { body } = this.state
+  render() {
+    let { body } = this.state;
 
     return (
-      <div className='box'>
-        <div className='title'>
-          <p className='title'>ALOL</p>
+      <div className="box">
+        <div className="title">
+          <p className="title">ALOL</p>
           <button>X</button>
         </div>
-        <div className='body'>
-          <p className='title'>Welcome {this.state.username}!</p>
-          <div className='inner'>
+        <div className="body">
+          <p className="title">Welcome {this.state.username}!</p>
+          <div className="inner">
             {this.state.postList.map(entry => {
               return (
                 <div id={entry.body}>
                   <p>{`${entry.username}: ${entry.body}`}</p>
                 </div>
-              )
+              );
             })}
           </div>
           <input
-            type='text'
+            type="text"
             value={body}
-            name='body'
+            name="body"
             onChange={this.handleChange}
           />
           <button onClick={this.handleSubmit}>Submit</button>
         </div>
       </div>
-    )
+    );
   }
 }
 
@@ -76,25 +76,22 @@ class messageBox extends Component {
  * CONTAINER
  */
 const mapState = state => {
-  return { username: 'Noah' }
-}
+  return { username: 'Edwin' };
+};
 
 const mapDispatch = dispatch => {
-  return {}
-}
+  return {};
+};
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
 const MessageBoxConnect = compose(
   withRouter,
   withFirebase,
-  connect(
-    mapState,
-    mapDispatch
-  )
-)
+  connect(mapState, mapDispatch)
+);
 
-export default MessageBoxConnect(messageBox)
+export default MessageBoxConnect(messageBox);
 
 /**
  * PROP TYPES
