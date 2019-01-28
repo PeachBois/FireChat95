0f2376acbd82c2c711431afd54ac518db577a5eb
Edwin Kim <edwin.k.kim@gmail.com>
2019-01-28 10:25:51 -0600

    Added Google Login with Firebase

--- client/components/login.js
+++ client/components/login.js
@@ -1,48 +1,54 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { withRouter, Route, Switch } from 'react-router-dom'
-import { withFirebase } from '../Firebase/index'
-import { compose } from 'recompose'
-import firebase from 'firebase'
-import { me } from '../store/user'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter, Route, Switch } from 'react-router-dom';
+import { withFirebase } from '../Firebase/index';
+import { compose } from 'recompose';
+import firebase from 'firebase';
+import { me } from '../store/user';
+import SignInGoogle from './SignInGoogle';
 
 class Login extends Component {
-  constructor () {
-    super()
+  constructor() {
+    super();
     this.state = {
       username: ''
-    }
+    };
   }
 
   handleChange = evt => {
-    this.setState({ [evt.target.name]: evt.target.value })
-  }
+    this.setState({ [evt.target.name]: evt.target.value });
+  };
   handleSubmit = evt => {
-    this.props.me(this.state.username)
-    this.props.history.push('/chat')
-  }
+    this.props.me(this.state.username);
+    this.props.history.push('/chat');
+  };
 
-  render () {
-    let { username } = this.state
+  render() {
+    let { username } = this.state;
 
     return (
-      <div className='box'>
-        <div className='title'>
-          <p className='title'>ALOL</p>
+      <div className="box">
+        <div className="title">
+          <p className="title">ALOL</p>
           <button>X</button>
         </div>
-        <div className='body'>
-          <p className='title'>Please Set Username!</p>
+        <div className="body">
+          <p className="title">Please Set Username!</p>
           <input
-            type='text'
+            type="text"
             value={username}
-            name='username'
+            name="username"
             onChange={this.handleChange}
           />
-          <button onClick={this.handleSubmit}>Set</button>
+          <button type="submit" onClick={this.handleSubmit}>
+            Login
+          </button>
+        </div>
+        <div className="body">
+          <SignInGoogle handleSubmit={this.handleSubmit} />
         </div>
       </div>
-    )
+    );
   }
 }
 
@@ -50,27 +56,24 @@ class Login extends Component {
  * CONTAINER
  */
 const mapState = state => {
-  return {}
-}
+  return {};
+};
 
 const mapDispatch = dispatch => {
   return {
     me: name => dispatch(me(name))
-  }
-}
+  };
+};
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
 const MessageBoxConnect = compose(
   withRouter,
   withFirebase,
-  connect(
-    mapState,
-    mapDispatch
-  )
-)
+  connect(mapState, mapDispatch)
+);
 
-export default MessageBoxConnect(Login)
+export default MessageBoxConnect(Login);
 
 /**
  * PROP TYPES
