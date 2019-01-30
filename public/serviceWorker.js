dce3a498b140f88cfcd04ebb2fd78e04cd0ff883
Edwin Kim <edwin.k.kim@gmail.com>
2019-01-30 14:39:29 -0600

    PWA: service worker offline caching works

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -5,53 +5,75 @@ const CACHE_NAME = 'pwa-app-cache';
 
 console.log('++++++++HELLO!!!! from SERVICE WORKER!!!>>>>>>');
 
-//Deleting old chaches
-self.addEventListener('activate', event => {
-  console.log('ACTIVATING!!!!');
-  console.log('EVENT >>>> ', event);
-  const currentCacheList = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(keyList =>
-      Promise.all(
-        keyList.map(key => {
-          if (!currentCacheList.includes(key)) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-});
+//"self": Requesting access to the service worker at the background
 
 //Triggers when user starts the app.
 self.addEventListener('install', event => {
-  console.log('INSTALLING!!!!');
-  console.log('EVENT >>>> ', event);
+  console.log('INSTALLING Service worker!!');
+  console.log('Install EVENT >>>> ', event);
   if (doCache) {
-    event.waitUntil(caches.open(CACHE_NAME)).then(cache => {
-      fetch('manifest.json')
-        .then(response => {
-          response.json();
-        })
-        .then(assets => {
-          //Edwin's comment: not sure about this part.. Comeback..
-          //Caches initial page and './index.js??
-          //Could also cache assets like CSS and images
-          const urlsToCache = ['/', assets['bundle.js']];
-          cache.addAll(urlsToCache);
-        });
-    });
+    console.log('Inside doCache if statement==caches=>', caches);
+    event.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log('Service Worker: Pre-cache App Shell!!', cache);
+
+        const urlsToCache = ['/', '/index.html'];
+        cache.addAll(urlsToCache);
+
+        // fetch('manifest.json')
+        //   .then(response => {
+        //     response.json();
+        //   })
+        //   .then(assets => {
+        //     //Edwin's comment: not sure about this part.. Comeback..
+        //     //Caches initial page and './index.js??
+        //     //Could also cache assets like CSS and images
+        //     const urlsToCache = ['/', assets['index.html']];
+        //     cache.addAll(urlsToCache);
+        //   });
+      })
+    );
   }
 });
 
-//In here, intercepts the network request and serves up the matching files
+//Deleting old chaches
+self.addEventListener('activate', event => {
+  console.log('ACTIVATING Service worker!!');
+  console.log('Activate EVENT >>>> ', event);
+  return self.clients.claim();
+
+  // const currentCacheList = [CACHE_NAME];
+  // event.waitUntil(
+  //   caches.keys().then(keyList =>
+  //     Promise.all(
+  //       keyList.map(key => {
+  //         if (!currentCacheList.includes(key)) {
+  //           return caches.delete(key);
+  //         }
+  //       })
+  //     )
+  //   )
+  // );
+});
+
+//In here, intercepts the network request and serves up the matching files.
+//Fetch is triggered by the web application of the actual page unlike 'install' & 'activate' where they are triggered by the browser when service workers are being installed or was activated.
 self.addEventListener('fetch', event => {
-  console.log('FETCHING!!!!');
-  console.log('EVENT >>>> ', event);
+  console.log('FETCHING Service worker!!');
+  console.log('Fetch EVENT >>>> ', event);
   if (doCache) {
     event.respondWith(
       caches.match(event.request).then(response => {
-        return response || fetch(event.request);
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request).then(serverRes =>
+            caches.open('dynamic-cache').then(cache => {
+              cache.put(event.request.url, serverRes.clone());
+              return serverRes;
+            })
+          );
+        }
       })
     );
   }
