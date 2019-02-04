402e75a8a28229cc12e5bfc26ba94c51ef513f41
Noah <noahbehtel@gmail.com>
2019-02-04 10:44:53 -0600

    conditional

--- client/components/SignIn.js
+++ client/components/SignIn.js
@@ -59,15 +59,35 @@ class SignInScreenBase extends Component {
         }
       })
   }
+ getOs() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  
+        // Windows Phone must come first because its UA also contains "Android"
+      if (/windows phone/i.test(userAgent)) {
+          return "Windows Phone";
+      }
+  
+      if (/android/i.test(userAgent)) {
+          return "Android";
+      }
+  
+      // iOS detection from: http://stackoverflow.com/a/9039885/177710
+      if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+          return "iOS";
+      }
+  
+      return "unknown";
+  }
   componentWillUnmount () {
     this.unregisterAuthObserver()
+    
   }
   render () {
     if (!this.state.isSignedIn) {
       return (
         <div className='logBox'>
           {'matchMedia' in window &&
-window.matchMedia('(display-mode: standalone)').matches ? (
+window.matchMedia('(display-mode: standalone)').matches && (This.getOs() === 'iOS') ? (
             <button onClick={this.AnonLog} className='anon'>
               <h3>Anonymous Login</h3>
             </button>
